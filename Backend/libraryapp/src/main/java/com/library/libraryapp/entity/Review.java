d67f21ebe249559b4d5e54537ed71833521971a2
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-07-01 10:48:48 +0430

    added review entity in spring boot

---
+++ Backend/libraryapp/src/main/java/com/library/libraryapp/entity/Review.java
@@ -0,0 +1,42 @@
+package com.library.libraryapp.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "review")
+@Data
+
+public class Review {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "date")
+    @CreationTimestamp
+    private Date date;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "book_id")
+    private long bookId;
+
+    @Column(name = "review_description")
+    private String reviewDescription;
+
+}
