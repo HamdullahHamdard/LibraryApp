f9c47559865e1dcf1f35114214332c0bbf549c07
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-07-03 12:02:59 +0430

    Auth configuration in front end/react

---
+++ Frontend/react-library/src/Auth/LoginWidget.jsx
@@ -0,0 +1,26 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../layouts/Utils/SpinnerLoading";
+import { Redirect } from "react-router-dom";
+import OktaSigninWidget from "./OktaSigninWidget";
+
+const LoginWidget = ({ config }) => {
+    const [oktaAuth, authState] = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    };
+    const onError = (err) => {
+        console.log("sign in error: ", err);
+    };
+
+    if (!authState) {
+        return <SpinnerLoading />;
+    }
+
+    return authState.isAuthenticated ? (
+        <Redirect to={{ pathname: "/" }} />
+    ) : (
+        <OktaSigninWidget onError={onError} onSuccess={onSuccess} config={config}/>
+    );
+};
+
+export default LoginWidget;
