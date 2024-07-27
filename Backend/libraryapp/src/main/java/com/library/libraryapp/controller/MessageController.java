6ce9c8611f683788d06fcb79a4c1ddc8ae99a386
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-07-28 00:05:28 +0430

    added put message controller for api

--- Backend/libraryapp/src/main/java/com/library/libraryapp/controller/MessageController.java
+++ Backend/libraryapp/src/main/java/com/library/libraryapp/controller/MessageController.java
@@ -2,12 +2,14 @@ package com.library.libraryapp.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.library.libraryapp.entity.Message;
+import com.library.libraryapp.requestmodels.AdminQuestionRequest;
 import com.library.libraryapp.service.MessageService;
 import com.library.libraryapp.utils.ExtractJWT;
 
@@ -25,5 +27,16 @@ public class MessageController {
     public void postMessage(@RequestHeader(value = "Authorization") String token, @RequestBody Message message) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messageService.postMessaging(message, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token, @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if(admin == null || !admin.equals("admin")){
+            throw new Exception("Adminastration page only");
+        }
+
     }
 }
