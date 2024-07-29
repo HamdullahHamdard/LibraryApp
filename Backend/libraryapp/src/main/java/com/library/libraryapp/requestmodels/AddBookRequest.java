d8b10eedd1dec674cb71d23c1d316beb485556fb
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-07-29 14:46:55 +0430

    add book request in springboot request models

---
+++ Backend/libraryapp/src/main/java/com/library/libraryapp/requestmodels/AddBookRequest.java
@@ -0,0 +1,15 @@
+package com.library.libraryapp.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class AddBookRequest {
+
+    private String title;
+    private String author;
+    private String description;
+    private int copies;
+    private String category;
+    private String img;
+    
+}
