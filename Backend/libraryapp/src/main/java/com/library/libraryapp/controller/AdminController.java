64fe2f197b5a59aa9a88d90340f47767a811a8d1
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-07-29 15:22:14 +0430

    create constractor and post book method in Admin Controller

--- Backend/libraryapp/src/main/java/com/library/libraryapp/controller/AdminController.java
+++ Backend/libraryapp/src/main/java/com/library/libraryapp/controller/AdminController.java
@@ -1,12 +1,38 @@
 package com.library.libraryapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.library.libraryapp.requestmodels.AddBookRequest;
+import com.library.libraryapp.service.AdminService;
+import com.library.libraryapp.utils.ExtractJWT;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
     
+
+    private AdminService adminService;
+    
+
+    @Autowired
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token, @RequestBody AddBookRequest addBookRequest) throws Exception {
+
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
 }
