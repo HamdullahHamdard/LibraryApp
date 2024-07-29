f723d106137d6bdd5c740d06b9eab0d48c064d04
HamdullahHamdard <hamdullahhamdard74@gmail.com>
2024-07-29 15:15:59 +0430

    create Admin Controller and it annotation

---
+++ Backend/libraryapp/src/main/java/com/library/libraryapp/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.library.libraryapp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    
+}
